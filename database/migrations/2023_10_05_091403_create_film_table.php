ae0d541337d0cac0ed00841f88fd05f4f70127d5
Laska Dwi Prayoga <laskaprayoga@gmail.com>
2023-10-05 16:36:35 +0700

    first commit

---
+++ database/migrations/2023_10_05_091403_create_film_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('film', function (Blueprint $table) {
+            $table->id();
+            $table->integer('judul');
+            $table->text('ringkasan');
+            $table->integer('tahun');
+            $table->string('poster');
+            $table->foreignId('genre_id')->constrained('id')->on('genre');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('film');
+    }
+};
